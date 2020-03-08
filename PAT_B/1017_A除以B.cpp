895d544adf3faafc5256fca75d1b911cb658314c
kirlaw <mqdz@foxmail.com>
2020-03-08 20:39:56 +0800

    1017 A除以B

--- PAT_B/1017_A除以B.cpp
+++ PAT_B/1017_A除以B.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main(){
     string a;
-    int b; //除数
-    int q,r; //商 余数
-    cin>>a>>b;
-    q=(a[0]-'0')/b;
-    if(q!=0&&a.length()>1 || a.length()==1){
-        cout<<q;
+    int b; //1位除数
+    int q, r; //商 余数
+    cin >> a >> b;
+    q = (a[0] - '0') / b;
+    if (q != 0 && a.length() > 1 || a.length() == 1){
+        cout << q;
     }
-    r=(a[0] - '0') % b;
-    for(int i=1;i<a.length();i++){
-        r=r*10+(a[i]-'0');
-        q=r/b;
-        cout<<q;
-        r=r%b;
+    r = (a[0] - '0') % b;
+    for (int i = 1; i < a.length(); i++) {
+        r = r * 10 + (a[i] - '0');
+        q = r / b;
+        cout << q;
+        r = r % b;
     }
-    cout<<" "<<r;
+    cout << " " << r;
     return 0;
 }
