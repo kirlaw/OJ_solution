432e6053a7ee61468b6fdaf4f4ed2eaf43b7bb0b
kirlaw <mqdz@foxmail.com>
2020-03-08 20:39:15 +0800

    1010 一元多项式求导

--- PAT_B/1010_一元多项式求导.cpp
+++ PAT_B/1010_一元多项式求导.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int i=0;
     int coef,expo;
+    int flag=0;
     while(cin>>coef>>expo){      
         if (expo==0){
             break;
@@ -12,11 +13,16 @@ int main(){
         
         if(i!=0){
             cout<<" "<<coef*expo<<" "<<expo-1;
+            flag=1;
         }
         else{
             cout<<coef*expo<<" "<<expo-1;
+            flag=1;
         }
         i++;
+    }
+    if (flag==0) {
+        cout<<"0 0";
     }
     return 0;
 }
