d72337051400b1489c90dde9e9f6707d20573e79
kirlaw <mqdz@foxmail.com>
2020-03-11 18:22:49 +0800

    习题2-5 求平方根序列前N项和

---
+++ 浙大版《C语言程序设计（第3版）》题目集/习题2-5_求平方根序列前N项和.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    double sum=0;
+    for(int i=1;i<=n;i++){
+        sum+=sqrt(i);
+    }
+    printf("sum = %.2f",sum);
+    return 0;
+}
